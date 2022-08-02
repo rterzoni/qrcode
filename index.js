dfcb53b1f8b2e9bc2ffc4f875f7b8b76061066ee
rterzoni <rafael.terzoni@gmail.com>
2022-08-02 12:05:34 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World! testing 123");
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World! testing");
 });
 
@@ -9,3 +21,15 @@ const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+function response(req, res) {
+    fs.readFile(__dirname + '/index.html',
+    function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Failed to load file index.html');
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}
